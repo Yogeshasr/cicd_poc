6fe8e0a37fff19b68e2c519690e95605b2102bb2
yogesha.sr <yogesha.sr@accord-soft.com>
2023-07-18 18:54:05 +0530

    test

--- app.js
+++ app.js
@@ -9,5 +9,4 @@ app.get("/test", (req, res) => {
   res.send("test page");
 });
 
-const port = process.env.port || 8080;
-app.listen(port, console.log("server is running on 5500"));
+app.listen(8080, console.log("server is running on 5500"));
